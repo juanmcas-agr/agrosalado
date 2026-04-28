de6c74e33c00c997e75d90f09916094298f32789
juanmcas-agr <juanmanueluranga@gmail.com>
2026-04-28 10:26:18 -0300

    Update tc.js

--- netlify/functions/tc.js
+++ netlify/functions/tc.js
@@ -1,12 +1,12 @@
 exports.handler = async function() {
-  const res = await fetch('https://dolarapi.com/v1/ar/dolares');
+  const res = await fetch('https://api.argentinadatos.com/v1/cotizaciones/dolares');
   const data = await res.json();
 
   const casas = ['oficial', 'blue', 'bolsa', 'mayorista'];
   const resultado = {};
   for (const casa of casas) {
     const d = data.find(x => x.casa === casa);
-    if (d) resultado[casa] = { compra: d.compra, venta: d.venta, fecha: d.fechaActualizacion };
+    if (d) resultado[casa] = { compra: d.compra, venta: d.venta, fecha: d.fecha };
   }
 
   return {
